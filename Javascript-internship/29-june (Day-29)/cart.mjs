3e61218eb9de28e107f831abaf2908ba1b8f0eca
Pruthviraj Bhosale <bpruthviraj248@gmail.com>
2026-06-29 19:55:42 +0530

    ES6 features practice

---
+++ Javascript-internship/29-june (Day-29)/cart.mjs
@@ -0,0 +1,9 @@
+import {product,produtDetails} from './product.js'
+import a from './product.js'
+
+
+console.log("Cart Page");
+// // console.log(product);
+// console.log(produtDetails());
+// console.log(a());
+produtDetails();
---
+++ Javascript-internship/29-june (Day-29)/product.js
@@ -0,0 +1,14 @@
+export let product={
+    
+    pname:"Watch",
+    cname:"Rolex",
+    price:5000
+}
+
+export function productDetails(){
+    console.log("Watch,600s0");
+}
+
+export default function book(){
+    console.log("IKIGAI is written by abc");
+}
