5c1abbaeff5f92e918e2646de97d8776ad7c1b21
Pruthviraj Bhosale <bpruthviraj248@gmail.com>
2026-07-01 23:43:29 +0530

    React basic

---
+++ ReactJS/01-jully (Day-31)/reat_basic/src/Test.jsx
@@ -0,0 +1,13 @@
+export default function Test(){
+    let a = 10;
+    let b = 20;
+    return <div>
+        <h1>Hello Form Test .jsx</h1>
+        <h1>Addition: {a+b}</h1>
+        <h1>Subtraction: {a-b}</h1>
+        <h1>Multilication: {a*b}</h1>
+        <input type="text" placeholder="Enter name:" />
+
+
+    </div>
+}
